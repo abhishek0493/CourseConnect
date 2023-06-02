f4974d7f624a44345eaaca56b21e28def6a0aa21
Abhishek Padaya <abhishek0493@gmail.com>
2023-06-02 19:56:00 +0100

    removed unused constants

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,11 @@
-const bcrypt = require('bcrypt');
 const db = require('../db');
-const { userCreationSchema } = require('../utils/validation');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-const userTypes = require('../utils/constants');
 
-const getAllUsers = catchAsync(async (req, res, next) => {
+const getAllUsers = catchAsync(async (req, res) => {
   const users = await db.select().from('users');
   if (!users) {
-    return next(new AppError('No Users found', 404));
+    return res.status(400).json({ success: false, message: 'No users found' });
   }
   res.status(200).json({
     data: users,
