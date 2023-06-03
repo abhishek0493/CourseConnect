b491687f7ff163ae98cf66843cffe3393e44f1f7
Abhishek Padaya <abhishek0493@gmail.com>
2023-06-03 21:27:05 +0100

    Signup for react

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
 import Signup from './components/auth/Signup';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [userTypes, setUserTypes] = useState([]);
+
+  const fetchUserTypes = () => {
+    axios
+      .get('http://localhost:8000/api/v1/users/categories')
+      .then((response) => {
+        // console.log(response.data);
+        setUserTypes(response.data);
+      })
+      .catch((error) => {
+        // Handle the error
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchUserTypes();
+  }, []);
+
   return (
     <div className="App">
-      <Signup />
+      <Signup userTypes={userTypes} />
     </div>
   );
 }
--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,25 +1,84 @@
-const Signup = () => {
+import './signup.css';
+import vector from './signup2.jpg';
+
+const Signup = (props) => {
+  const arrData = Object.entries(props);
+  // console.log(arrData);
   return (
-    <div className="container">
-      <h1>Signup</h1>
-      <form>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email address
-          </label>
-          <input type="email" className="form-control" id="email" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input type="password" className="form-control" id="password" />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Sign up
-        </button>
-      </form>
-    </div>
+    <section className="signup-super row p-4 m-5">
+      <div className="image-box col-md-6 p-3">
+        <img src={vector} className="illustration" />
+      </div>
+      <div className="sign-up-form col-md-6 p-3">
+        <h1 className="mb-4">Signup</h1>
+        <form className="row g-3">
+          <div className="col-md-6">
+            <label htmlFor="name" className="form-label">
+              Name
+            </label>
+            <input type="text" className="form-control" id="name" />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="inputPassword4" className="form-label">
+              Email
+            </label>
+            <input type="email" className="form-control" id="inputPassword4" />
+          </div>
+          <div className="col-6">
+            <label htmlFor="inputAddress" className="form-label">
+              Password
+            </label>
+            <input type="password" className="form-control" id="password" />
+          </div>
+          <div className="col-6">
+            <label htmlFor="inputAddress" className="form-label">
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="confirm_password"
+            />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="inputState" className="form-label">
+              Profession
+            </label>
+            <select id="inputState" className="form-select">
+              {arrData.map((item, index) => {
+                <option>{item[1].name}</option>;
+              })}
+              {/* <option>Student</option>
+              <option>Instrutor/Professor</option>
+              <option>Working Professional</option> */}
+            </select>
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="inputCity" className="form-label">
+              University/Company Name (Optional)
+            </label>
+            <input type="text" className="form-control" id="inputCity" />
+          </div>
+          <div className="col-12">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                id="gridCheck"
+              />
+              <label className="form-check-label" htmlFor="gridCheck">
+                Give Consent
+              </label>
+            </div>
+          </div>
+          <div className="col-12">
+            <button type="submit" className="btn btn-primary">
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 };
 
