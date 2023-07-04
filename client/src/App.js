8c403e93670dcb64dc3bfcd5d009874c133b33cf
Abhishek Padaya <abhishek0493@gmail.com>
2023-07-04 17:32:51 +0100

    Routes refactored - Layout incl

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,15 @@
-import Signup from './pages/signup/Signup';
 import SignupNew from './pages/signup/SignupNew';
 import Home from './pages/home/home';
-import Navbar from './components/navbar/navbar';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
 import Consent from './pages/consent/consent';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import Dashboard from './pages/dashboard/dashboard';
 
 function App() {
   const [userTypes, setUserTypes] = useState([]);
-  const [loggedIn, setLoggedIn] = useState(false);
   const fetchUserTypes = () => {
     axios
       .get('http://localhost:8000/api/v1/users/categories')
@@ -31,12 +29,12 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="consent" element={<Consent />} />
-        <Route path="sign-up" element={<SignupNew userTypes={userTypes} />} />
-        <Route path="dashboard" element={<Dashboard />}></Route>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="consent" element={<Consent />} />
+          <Route path="sign-up" element={<SignupNew userTypes={userTypes} />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/components/layout/community/create/createCommunity.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const createCommunity = () => {
+  return <div>createCommunity</div>;
+};
+
+export default createCommunity;
